7c29e742aa085dc0a4be7e2a1ddf3f2d14248da7
Johnny Ha <johnny.ha.github@gmail.com>
2024-03-13 09:58:05 -0700

    added a todo list

---
+++ daily-todo.js
@@ -0,0 +1,12 @@
+/**
+
+* TODO:
+
+  * ? : Finish the DB Schema
+    * * Tables, Relationships, Columns, etc.
+  * ? : Finish Setting Up AWS
+  * ? : Install Boto3 to backend
+  * ? : Start Sourcing Seeder Images
+  * ? :
+
+**/
