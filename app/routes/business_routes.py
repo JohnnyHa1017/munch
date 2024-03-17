ab31c2b8827d81a72abf1714184fdc13d3e6921c
Tyler_Han <tylerhan1226@gmail.com>
2024-03-17 15:38:09 -0700

    add create business route

--- app/routes/business_routes.py
+++ app/routes/business_routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, jsonify
-from app.models import Business
+from flask import Blueprint, jsonify, request
+from app.models import Business, db
 from app.forms.business_form import CreateBusiness
-from flask_login import login_required
+from flask_login import login_required, current_user
 import json
 
 bp = Blueprint('business_routes', __name__, url_prefix='/business')
@@ -29,16 +29,18 @@ def one_business(id):
 
 #create business
 @bp.route('/new', methods=['GET', 'POST'])
-# @login_required
+@login_required
 def create_business():
     form = CreateBusiness()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    print('current_user ==>', current_user)
     if form.validate_on_submit():
-        data = Business()
+        params = { "owner_id": current_user.id}
+        data = Business(**params)
         form.populate_obj(data)
         db.session.add(data)
         db.session.commit()
-        print('Here!')
-        return data
-    return "Cannot create"
+        return data.to_dict()
+    return form.errors
 
 
