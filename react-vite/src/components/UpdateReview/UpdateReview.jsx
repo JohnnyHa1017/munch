19e822e3b3d73e957475b124ed3cce43bfae9429
Tyler_Han <tylerhan1226@gmail.com>
2024-03-20 16:24:48 -0700

    update review

--- react-vite/src/components/UpdateReview/UpdateReview.jsx
+++ react-vite/src/components/UpdateReview/UpdateReview.jsx
@@ -1,35 +1,37 @@
 import { useSelector, useDispatch } from "react-redux"
-import  CreateNewReview  from "../CreateReview/CreateReview"
+import CreateNewReview from "../CreateReview/CreateReview"
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { updateReviewThunk, businessReviewThunk } from "../../redux/reviews";
 
 const UpdateReview = () => {
 
-  const reviews = useSelector((state) => state.reviews)
-  console.log('reviews ==>', reviews)
-  const buttonName = 'Update a Review'
-  const dispatch = useDispatch();
-  const { reviewId, businessId } = useParams()
+    const reviews = useSelector((state) => state.reviews)
+    console.log('reviews ==>', reviews)
+    const buttonName = 'Update a Review'
+    const dispatch = useDispatch();
+    const { reviewId, businessId } = useParams()
 
-  if (!reviews || !reviews.Review || !reviews.ReviewImage) {
-    return <h1>Loading...</h1>;
-  }
+    let review
+    if (reviews && reviews.Review) {
+        review = reviews.Review.find(eachReview => eachReview.id == reviewId)
+        console.log('review ==>', review)
+    }
 
-  const review = state.Review.find(eachReview => eachReview.id == reviewId)
+    useEffect(() => {
+        dispatch(businessReviewThunk(businessId))
+    }, [businessId, dispatch])
 
-  console.log('review ==>', review)
+    //   if (!reviews || !reviews.Review || !reviews.ReviewImage) {
+    //     return <h1>Loading...</h1>;
+    //   }
 
-  useEffect(() => {
-    dispatch(businessReviewThunk(businessId))
-  }, [businessId, dispatch])
-
-
-  return (
-    <>
-      <h1>Update Review</h1>
-      <CreateNewReview />
-    </>
+    return (
+        <>
+            <h1>Update Review</h1>
+            <CreateNewReview review={review} button={buttonName} />
+            {/* <h1>Here's the review{review}</h1> */}
+        </>
     )
 }
 
