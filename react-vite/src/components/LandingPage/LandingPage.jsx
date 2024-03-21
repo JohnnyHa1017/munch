a6d3db382cea4da3c5c9efbefaf15d97911c4d41
Jasmine <jasminetruong97@gmail.com>
2024-03-21 10:46:08 -0700

    add users obj

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import {  useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { landingPageThunk } from '../../redux/business'
+import { NavLink } from 'react-router-dom';
 // import { useModal } from "../../context/Modal";
 // import { landingPageThunk } from '../../redux/business'
 // import { SignupFormModal } from '../SignupFormModal'
@@ -11,17 +12,77 @@ import { landingPageThunk } from '../../redux/business'
 export default function LandingPage() {
 
     const dispatch = useDispatch()
-
     const data = useSelector((state) => state.business)
-    console.log(data)
+    const state = useSelector(state => console.log(state))
+
+    const businessArray = data.Business
+    const reviewsArray = data.Review
+    // const amenitiesArray = data.Amen
+
+
+    console.log('DATA ---->', data)
 
     useEffect(() => {
         dispatch(landingPageThunk())
     }, [dispatch])
 
+    if(!data || !data.Review || !data.Business){
+        return <div>Loading...</div>
+    }
+
+    const sixreviews = []
+    for(let i=0; i < 6; i++){
+        sixreviews.push(reviewsArray[i])
+    }
+    console.log(sixreviews, 'arr of 6 reviews')
+
+    // Helper func: Business avg star rating by id
+    function businessAvgRating(businessId){
+        let avgRating = 0
+        let numRev = 0
+        for (let review of reviewsArray){
+            if(review.business_id == businessId){
+                numRev++
+                avgRating += review.star
+            }
+        }
+        return avgRating/numRev
+    }
+
     return (
         <>
             <h1>Landing Page!</h1>
+            <div className='recent-activity-container'>
+                <h2 className='recent-actity-text'>Recent Activity</h2>
+                <div className='recent-reviews'>
+                    {sixreviews.map(review => (
+                        <div className='landing-business-review-container'>
+                            <div className='review-user'>
+                                <p>{} wrote a review</p>
+                            </div>
+                            <div className='landing-business-container'>
+                                <p className='landing-business-name'>{data?.Business[review.business_id].title}</p>
+                                <p className='landing-business-rating'>{businessAvgRating(data?.Business[review.business_id].id)}</p>
+                                <p className='landing-business-price-rating'>{data?.Business[review.business_id].price_rating}</p>
+                            </div>
+                            <p>{review.review}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className='landing-categories'>
+                    <h2>Categories</h2>
+                    <div className='landing-category-container'>
+                        <NavLink to='/' className='landing-category'>Dinner</NavLink>
+                        <NavLink to='/' className='landing-category'>Take Out</NavLink>
+                        <NavLink to='/' className='landing-category'>Good For Groups</NavLink>
+                        <NavLink to='/' className='landing-category'>Brunch</NavLink>
+                        <NavLink to='/' className='landing-category'>Dessert</NavLink>
+                        <NavLink to='/' className='landing-category'>Open Late</NavLink>
+                        <NavLink to='/' className='landing-category'>Fine Dining</NavLink>
+                        <NavLink to='/' className='landing-category'>Casual Dining</NavLink>
+                    </div>
+                </div>
+            </div>
         </>
 
     )
