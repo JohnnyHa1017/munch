bbe33c88617aa04cfebc9f736446a005f210e226
Jasmine <jasminetruong97@gmail.com>
2024-03-21 16:07:29 -0700

    manage and nav bar

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,14 +1,27 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import { useSelector } from 'react-redux';
 import "./Navigation.css";
 
 function Navigation() {
+  const user = useSelector(state => state.session.user)
+
   return (
-    <ul>
+    <ul className='nav-bar-ul'>
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
-
+      {user && (
+        <>
+          <li>
+            <NavLink to='/business/new'>Create a New Business</NavLink>
+          </li>
+          <li>
+            <NavLink to={`/user/${user.id}/business`}>Manage Your Business</NavLink>
+          </li>
+        </>
+        )
+      }
       <li>
         <ProfileButton />
       </li>
