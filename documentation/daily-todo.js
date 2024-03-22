765fad6131513c1937c43a57cbffe82f68557609
Johnny Ha <johnny.ha.github@gmail.com>
2024-03-22 07:20:22 -0700

    plan of attack, redploy eod for submission

--- documentation/daily-todo.js
+++ documentation/daily-todo.js
@@ -1,29 +1,29 @@
 /**
 * TODO:
 
-  * : Finish the DB Schema
+  * //: Finish the DB Schema
   * //: Tables, Relationships, Columns, etc.
      * // Schema Approved Need to Upload to Repo
-      * TODO: https://discord.com/channels/@me/1216852228225372190/1217577128288190505
+      * //TODO: https://discord.com/channels/@me/1216852228225372190/1217577128288190505
 
-  * : Start Sourcing Seeder Images
+  * //: Start Sourcing Seeder Images
     * //: In Progress, Filling out Google Doc
-       * TODO: https://docs.google.com/document/d/1Vcom7naCxkWW4_XTghrqu4jtXQiSpKQU19tAlq8h2iQ/edit?usp=sharing
+       * //TODO: https://docs.google.com/document/d/1Vcom7naCxkWW4_XTghrqu4jtXQiSpKQU19tAlq8h2iQ/edit?usp=sharing
 
-       * : Create User Stories
+       * //: Create User Stories
       * //: Detailed Directions and Explanation per Route
-      * : Spots, Reviews :: Full CRUD
-      * : Menu, Amenities :: Half CR
-      * TODO: https://discord.com/channels/@me/1216852228225372190/1217600357987913859
+      * //: Spots, Reviews :: Full CRUD
+      * //: Menu, Amenities :: Half CR
+      * //TODO: https://discord.com/channels/@me/1216852228225372190/1217600357987913859
 
       * ? : Wireframes
-          // Optional
+          * // Optional
 
       * ? : Set Up Scrum Board
-          // Due Friday
+          * // Due Friday
 
           * TODO: FRIDAY - DAY 0 - STARTING DATABASE
-          * : Create Models
+          * //: Create Models
           * //: Users Model needs to add First Name, Last Name
           *   !: Need to test the setter getter in Business Model
 
@@ -46,7 +46,13 @@
           * ? : Define Routes
 
           * ? : Install Boto3 to backend
-            // Needs Follow Up
+            * // Needs Follow Up
+
+
+*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
+* ! ABOVE HAS BEEN COMPLETED OR LOW PRIORITY ! *
+*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
+
 
   // TODO: EXTREMELY URGENT - @ANYONE
   // !: BUSINESS/BUSINESSES IS NOT LOADING ANY OF OUR DATA
@@ -57,26 +63,33 @@
           // : Additional Logic of `img.preview === true` for one picture display icon
 
     // TODO: EXTREMELY URGENT - @JohnnyHa1017
-      // ! : Finish Setting Up AWS
-        // ?: NEEDS FOLLOW UP
-            * //: Added .env, .gitignore, and allowed permissions to IEM User
+    // ?: NEEDS FOLLOW UP
+      // *: NOT BLOCKED BY THE IMAGE SELECTOR ANYMORE
+        // *: IMAGE DOESNT SEEM TO ACTUALLY UPLOAD
+      * // : Finish Setting Up AWS
+        * //: Added .env, .gitignore, and allowed permissions to IEM User
 
-    // TODO: NEED TO FIX THE CATEGORY SELECTION BOX - @JohnnyHa1017
+    // TODO: NEED TO FIX THE CATEGORY SELECTION BOX
       // !: CREATE AND EDIT FORM FOR BUSINESS
         // *: IF MULTIPLE TO POPULATE MORE LINES
-          // ?: PREVIEW IMAGE BOX NEEDS TO ALSO POPULATE MORE LINES AND ACTUALLY WORK
-            // : ASSUME THE ISSUE FOR REVIEW IMAGE AS WELL
+          // ?: PREVIEW IMAGE BOX NEEDS TO ALSO POPULATE MORE LINES
+          // ?: ASSUME THE ISSUE FOR REVIEW IMAGE AS WELL
+          * //: AND ACTUALLY WORK
 
-      // !: FIX MENU IT SEEMS TO BE MESSING THINGS UP - @TylerHan1226
+    // !: FIX MENU - @TylerHan1226
       // *: I ended up fixing other issues but unsure if Menu is working or not now
         // : TIMESTAMP: 3:15AM - LAST NOTED
         // ?: Needs Follow Up
 
       // !: BUSINESS/:BUSINESSID
-          // *: NEED TO RENDER BUSINESS AMENITIES INFORMATION PLEASE - @jtruong97
-          // *: NEED TO RENDER BUSINESS MENU INFORMATION PLEASE - @TylerHan1226
+        // *: NEED TO RENDER BUSINESS AMENITIES INFORMATION PLEASE - @jtruong97
+        // *: NEED TO RENDER BUSINESS MENU INFORMATION PLEASE - @TylerHan1226
           * //: NEED TO RENDER BUSINESS IMAGES PLEASE
           * //: NEED TO RENDER BUSINESS INFORMATION PLEASE
+    // !: Something is causing render issues until we manually refresh
+      * TODO: Probably the Image Filter Array, I'm like 99% Sure of it
+      // *: IN OUR CASE, NEW BUSINESSES WILL CREATE BUT WE CAN'T PULL THEM UP ON SITE
+      // *: ON BUSINESSES WITH NO IMAGES IT IS NOT PULLING UP AT ALL
 
 
 **/
--- react-vite/src/components/BusinessForm/BusinessForm.jsx
+++ react-vite/src/components/BusinessForm/BusinessForm.jsx
@@ -4,13 +4,11 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { createNewBusinessThunk, updateBusinessThunk } from '../../redux/business'
 import './BusinessForm.css'
 
-
 const CreateNewBusiness = ({ buttonName, business }) => {
   const dispatch = useDispatch()
   const nav = useNavigate()
   const user = useSelector((state) => state.session.user)
   const { businessId } = useParams()
-
 
   const [title, setTitle] = useState(business?.title );
   const [address, setAddress] = useState(business?.address);
@@ -25,7 +23,6 @@ const CreateNewBusiness = ({ buttonName, business }) => {
   const [category, setCategory] = useState(business?.category);
   const [image, setImage] = useState(null);
   const [imageLoading, setImageLoading] = useState(false);
-  // const [previewImage, setPreviewImage] = useState(business?.previewImage);
   const [mondayopen, setMondayOpen] = useState('')
   const [mondayclose, setMondayClose]= useState('')
   const [tuesdayopen, setTuesdayOpen] = useState('')
@@ -43,48 +40,47 @@ const CreateNewBusiness = ({ buttonName, business }) => {
   const [validations, setValidations] = useState({})
   const [submitted, setSubmitted] = useState(false)
 
+useEffect(() => {
+  const errors = {}
+  if (!user) {
+    nav('/')
+  }
+  if (!title || (title.length > 50)) {
+    errors.title = 'Title is required and can only be under 50 characters.'
+  }
+  if (!address || (address.length > 50)) {
+    errors.address = 'Address is required and can only be under 50 characters.'
+  }
+  if (!city || city.length > 30) {
+    errors.city = 'City is required and can only be under 30 characters.'
+  }
+  if (!state || (state.length > 20)) {
+    errors.state = 'State is required and can only be under 20 characters.'
+  }
+  if (!country || (country.length > 20)) {
+    errors.county = 'Country is required and can only be under 20 characters.'
+  }
+  if (!lat || (lat >= 90) || (lat <= -90)) {
+    errors.lat = 'Latitude must be between -90 and 90.'
+  }
+  if (!lng || (lng >= 180) || (lng <= -180)) {
+    errors.lng = 'Longitude must be between -180 and 180.'
+  }
+  if (!description || (description.length > 2000)) {
+    errors.description = 'Description is required and must be 2000 characters or less.'
+  }
+  if (!phone_number || (phone_number.length > 20)) {
+    errors.phone_number = "Phone Number is required and must be unique, can't be more than 20 characters."
+  }
+  if (!price_rating) {
+    errors.price_rating = "Price Rating is required and must be an integer of 1 to 5."
+  }
+  if (!category) {
+    errors.category = 'Atleast one category must be selected.'
+  }
 
-  useEffect(() => {
-    const errors = {}
-    if (!user) {
-      nav('/')
-    }
-    if (!title || (title.length > 50)) {
-      errors.title = 'Title is required and can only be under 50 characters.'
-    }
-    if (!address || (address.length > 50)) {
-      errors.address = 'Address is required and can only be under 50 characters.'
-    }
-    if (!city || city.length > 30) {
-      errors.city = 'City is required and can only be under 30 characters.'
-    }
-    if (!state || (state.length > 20)) {
-      errors.state = 'State is required and can only be under 20 characters.'
-    }
-    if (!country || (country.length > 20)) {
-      errors.county = 'Country is required and can only be under 20 characters.'
-    }
-    if (!lat || (lat >= 90) || (lat <= -90)) {
-      errors.lat = 'Latitude must be between -90 and 90.'
-    }
-    if (!lng || (lng >= 180) || (lng <= -180)) {
-      errors.lng = 'Longitude must be between -180 and 180.'
-    }
-    if (!description || (description.length > 2000)) {
-      errors.description = 'Description is required and must be 2000 characters or less.'
-    }
-    if (!phone_number || (phone_number.length > 20)) {
-      errors.phone_number = "Phone Number is required and must be unique, can't be more than 20 characters."
-    }
-    if (!price_rating) {
-      errors.price_rating = "Price Rating is required and must be an integer of 1 to 5."
-    }
-    if (!category) {
-      errors.category = 'Atleast one category must be selected.'
-    }
-
-    setValidations(errors)
-  }, [user, nav, title, address, city, state, country, lat, lng, description, phone_number, price_rating, category])
+  setValidations(errors)
+}, [user, nav, title, address, city, state, country, lat, lng, description, phone_number, price_rating, category])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -92,159 +88,158 @@ const CreateNewBusiness = ({ buttonName, business }) => {
     const formData = new FormData();
     formData.append("image", image);
 
-    let createSchedule = `Monday: ${mondayopen} - ${mondayclose},
-                          Tuesday: ${tuesdayopen} - ${tuesdayclose},
-                          Wednesday: ${wednesdayopen} - ${wednesdayclose},
-                          Thursday: ${thursdayopen} - ${thursdayclose},
-                          Friday: ${fridayopen} - ${fridayclose},
-                          Saturday: ${saturdayopen} - ${saturdayclose},
-                          Sunday: ${sundayclose} - ${sundayclose}
-                        `
+  let createSchedule = `Monday: ${mondayopen} - ${mondayclose},
+                        Tuesday: ${tuesdayopen} - ${tuesdayclose},
+                        Wednesday: ${wednesdayopen} - ${wednesdayclose},
+                        Thursday: ${thursdayopen} - ${thursdayclose},
+                        Friday: ${fridayopen} - ${fridayclose},
+                        Saturday: ${saturdayopen} - ${saturdayclose},
+                        Sunday: ${sundayclose} - ${sundayclose}
+                      `
 
-    let business ={
-      title,
-      address,
-      city,
-      state,
-      country,
-      description,
-      phone_number,
-      price_rating,
-      lat,
-      lng,
-      category,
-      schedule: createSchedule,
-      image
-    };
-
-    await Promise.resolve(formData);
-
-    if (!Object.keys(validations).length) {
-      if (!businessId) {
-        const createBusiness = await dispatch(createNewBusinessThunk(business));
-        if (createBusiness && createBusiness.id) {
-
-          setImageLoading(true);
-          nav(`/business/${createBusiness.id}`);
-        }
-      } else {
-        const updateBusiness = await dispatch(updateBusinessThunk(business, businessId));
-        if (updateBusiness) {
-
-          setImageLoading(true);
-          nav(`/business/${businessId}`);
-        }
-      }
-    }
+  let business ={
+    title,
+    address,
+    city,
+    state,
+    country,
+    description,
+    phone_number,
+    price_rating,
+    lat,
+    lng,
+    category,
+    schedule: createSchedule,
+    image
   };
 
+  await Promise.resolve(formData);
 
-  return (
-    <form
-      onSubmit={handleSubmit}
-      encType="multipart/form-data"
-      className='business-form'
-    >
-      <h2>Where is your business located?</h2>
-      <label>
-        Business Name :
-        <input
-          type='text'
-          name='title'
-          value={title}
-          placeholder='Business Name'
-          onChange={(e) => setTitle(e.target.value)}
-        ></input>
-      </label>
-      {validations.title && (<p>{validations.title}</p>)}
-      <label>
-        Address :
-        <input
-          type='text'
-          name='address'
-          value={address}
-          placeholder='Address'
-          onChange={(e) => setAddress(e.target.value)}
-        ></input>
-      </label>
-      {validations.address && (<p>{validations.address}</p>)}
-      <label>
-        City :
-        <input
-          type='text'
-          name='city'
-          value={city}
-          placeholder='City'
-          onChange={(e) => setCity(e.target.value)}
-        ></input>
-      </label>
-      {validations.city && (<p>{validations.city}</p>)}
-      <label>
-        State :
-        <input
-          type='text'
-          name='state'
-          value={state}
-          placeholder='State'
-          onChange={(e) => setState(e.target.value)}
-        ></input>
-      </label>
-      {validations.state && (<p>{validations.state}</p>)}
-      <label>
-        Country :
-        <input
-          type='text'
-          name='country'
-          value={country}
-          placeholder='Country'
-          onChange={(e) => setCountry(e.target.value)}
-        ></input>
-      </label>
-      {validations.country && (<p>{validations.country}</p>)}
-      <label>
-        Price :
-        <input
-          type='text'
-          name='price'
-          value={price_rating}
-          placeholder='Price'
-          onChange={(e) => setPrice(e.target.value)}
-        ></input>
-      </label>
-      {validations.price && (<p>{validations.price}</p>)}
-      <label>
-        Category : <br></br>
-        <input
-          list='categories'
-          name='category'
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        ></input>
-        <datalist id='categories'>
-          <option value='American'></option>
-          <option value='Asian Fusion'></option>
-          <option value='Bar'></option>
-          <option value='Brunch'></option>
-          <option value='Cafe'></option>
-          <option value='Casual'></option>
-          <option value='Cocktail Bar'></option>
-          <option value='Deli'></option>
-          <option value='Dessert'></option>
-          <option value='Dinner'></option>
-          <option value='Fast Food'></option>
-          <option value='Fine Dining'></option>
-          <option value='German'></option>
-          <option value='Indian'></option>
-          <option value='Italian'></option>
-          <option value='Japanese'></option>
-          <option value='Mexican'></option>
-          <option value='Palestinian'></option>
-          <option value='Pub'></option>
-          <option value='Seafood'></option>
-          <option value='Tapas'></option>
-        </datalist>
-      </label>
-      {validations.category && (<p>{validations.category}</p>)}
+  if (!Object.keys(validations).length) {
+    if (!businessId) {
+      const createBusiness = await dispatch(createNewBusinessThunk(business));
+      if (createBusiness && createBusiness.id) {
+
+        setImageLoading(true);
+        nav(`/business/${createBusiness.id}`);
+      }
+    } else {
+      const updateBusiness = await dispatch(updateBusinessThunk(business, businessId));
+      if (updateBusiness) {
+
+        setImageLoading(true);
+        nav(`/business/${businessId}`);
+      }
+    }
+  }
+};
+
+return (
+  <form
+    onSubmit={handleSubmit}
+    encType="multipart/form-data"
+    className='business-form'
+  >
+    <h2>Where is your business located?</h2>
+    <label>
+      Business Name :
+      <input
+        type='text'
+        name='title'
+        value={title}
+        placeholder='Business Name'
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
+    </label>
+    {validations.title && (<p>{validations.title}</p>)}
+    <label>
+      Address :
+      <input
+        type='text'
+        name='address'
+        value={address}
+        placeholder='Address'
+        onChange={(e) => setAddress(e.target.value)}
+      ></input>
+    </label>
+    {validations.address && (<p>{validations.address}</p>)}
+    <label>
+      City :
+      <input
+        type='text'
+        name='city'
+        value={city}
+        placeholder='City'
+        onChange={(e) => setCity(e.target.value)}
+      ></input>
+    </label>
+    {validations.city && (<p>{validations.city}</p>)}
+    <label>
+      State :
+      <input
+        type='text'
+        name='state'
+        value={state}
+        placeholder='State'
+        onChange={(e) => setState(e.target.value)}
+      ></input>
+    </label>
+    {validations.state && (<p>{validations.state}</p>)}
+    <label>
+      Country :
+      <input
+        type='text'
+        name='country'
+        value={country}
+        placeholder='Country'
+        onChange={(e) => setCountry(e.target.value)}
+      ></input>
+    </label>
+    {validations.country && (<p>{validations.country}</p>)}
+    <label>
+      Price :
+      <input
+        type='text'
+        name='price'
+        value={price_rating}
+        placeholder='Price'
+        onChange={(e) => setPrice(e.target.value)}
+      ></input>
+    </label>
+    {validations.price && (<p>{validations.price}</p>)}
+    <label>
+      Category : <br></br>
+      <input
+        list='categories'
+        name='category'
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      ></input>
+      <datalist id='categories'>
+        <option value='American'></option>
+        <option value='Asian Fusion'></option>
+        <option value='Bar'></option>
+        <option value='Brunch'></option>
+        <option value='Cafe'></option>
+        <option value='Casual'></option>
+        <option value='Cocktail Bar'></option>
+        <option value='Deli'></option>
+        <option value='Dessert'></option>
+        <option value='Dinner'></option>
+        <option value='Fast Food'></option>
+        <option value='Fine Dining'></option>
+        <option value='German'></option>
+        <option value='Indian'></option>
+        <option value='Italian'></option>
+        <option value='Japanese'></option>
+        <option value='Mexican'></option>
+        <option value='Palestinian'></option>
+        <option value='Pub'></option>
+        <option value='Seafood'></option>
+        <option value='Tapas'></option>
+      </datalist>
+    </label>
+    {validations.category && (<p>{validations.category}</p>)}
 <br></br>
       <label>
         Latitude :
