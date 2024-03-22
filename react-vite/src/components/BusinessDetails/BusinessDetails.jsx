04256296a93450dcc7dfdc1d7d674bf2eba9cce8
Tyler_Han <tylerhan1226@gmail.com>
2024-03-22 09:45:38 -0700

    update business dtl

--- react-vite/src/components/BusinessDetails/BusinessDetails.jsx
+++ react-vite/src/components/BusinessDetails/BusinessDetails.jsx
@@ -1,74 +1,128 @@
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { businessAmenitiesThunk, businessMenuThunk, specificBusinessThunk } from '../../redux/business'
+import { businessAmenitiesThunk, specificBusinessThunk } from '../../redux/business'
+import { menuByBusinessThunk } from '../../redux/menu'
 import { useParams } from 'react-router-dom'
 import { businessReviewThunk } from "../../redux/reviews";
+import './BusinessDetails.css'
+import { LuBean } from "react-icons/lu";
+import { BiGame, BiSolidGame, BiSolidBadgeDollar } from "react-icons/bi";
+import BusinessReviews from "../BusinessReviews/BusinessReviews";
 
-// TODO: Pass in reviews
-  // *: COMPLETED THANK YOU @MoonChopperr
+
+// TODO get current time and make Open/Closed dynamic
+
 
 export default function OneBusiness() {
   const dispatch = useDispatch()
-  const data = useSelector((state) => state.business)
+  const business = useSelector(state => state.business)
   const reviews = useSelector(state => state.reviews)
+  const menus = useSelector(state => state.menus)
   const { businessId } = useParams()
+
+
+
+
+  //get average start rating
+  let avgStarRating = 1
+  let priceRating = 1
+  let businessPreviewImg = {}
+  let businessCategory = ""
+  let businessSchedule = ""
+  let selectedBusiness = {}
+  if (reviews.Review && business && businessId && menus) {
+    avgStarRating = reviews.Review.reduce((acc, curr) => {
+      if ('star' in curr && typeof curr.star == 'number') {
+        return acc + curr.star
+      } else {
+        return acc
+      }
+    }, 0) / reviews.Review.length
+    const id = businessId
+    selectedBusiness = business[id]
+
+    if (selectedBusiness && menus.Business_Images) {
+      priceRating = selectedBusiness.price_rating
+      let businessCategoryStr = selectedBusiness.category
+      businessCategory = businessCategoryStr.split(',')[0].split('"')[1]
+      businessPreviewImg = menus.Business_Images.filter(img => img.business_id == businessId && img.preview == true)[0]
+      businessSchedule = selectedBusiness.schedule
+    }    
+    // console.log('business ==>', business)
+    console.log('menus ==>', menus)
+    console.log('reviews ==>', reviews.Review)
+    console.log('businessPreviewImg ==>', businessPreviewImg)
+    console.log('selectedBusiness ==>', business[id])
+    console.log('avgStarRating ==>', avgStarRating)
+
+  }
 
   useEffect(() => {
     dispatch(specificBusinessThunk(businessId))
-    dispatch(businessMenuThunk(businessId))
+    dispatch(menuByBusinessThunk(businessId))
     dispatch(businessAmenitiesThunk(businessId))
     dispatch(businessReviewThunk(businessId))
-  }, [businessId, dispatch])
+  }, [businessId, dispatch, businessSchedule])
 
-  if (!data[businessId] || !reviews.Review) {
-    return <div>Loading...</div>
-  }
-
-  const businessImages = data.Business_Images.filter((img) => img.business_id == businessId);
-  const reviewArr = reviews.Review
 
   return (
     <>
-      <h1 id='business-name'>{data[businessId].title}</h1>
-      <p>Address: <br></br>
-        {data[businessId].address}</p>
-      <p>City: <br></br>
-        {data[businessId].city}</p>
-      <p>State: <br></br>
-        {data[businessId].state}</p>
-      <p> Categories: <br></br>
-      {JSON.parse(data[businessId].category).join(', ')}
-      </p>
-      <p>Phone Number: <br></br>
-        {data[businessId].phone_number}</p>
-      <p>Price Rating: <br></br>
-        {data[businessId].price_rating}</p>
-      <p>Description: <br></br>
-        {data[businessId].description}</p>
-      <h2>Check out some photos of {data[businessId].title}</h2>
+      {business && reviews.Review ? (
+        <>
+          <div className="business-detail-header-container">
+            <div className="business-detail-header-img" style={{ backgroundImage: `url(${businessPreviewImg.url})`, height: '360px' }}>
+              <h1 className="business-detail-header-text">{business.title}</h1>
+              <div className="bd-star-rating-container">
+                {[...Array(Math.floor(avgStarRating))].map((_, index) => (
+                  <BiSolidGame key={index} className="bd-star" />
+                ))}
+                {avgStarRating % 1 !== 0 && <BiGame className="bd-star" />}
+                {[...Array(5 - Math.ceil(avgStarRating))].map((_, index) => (
+                  <LuBean key={index} className="bd-star" />
+                ))}
+                <h3 className="business-detail-header-text">
+                  {avgStarRating} ({reviews.Review.length} reviews)
+                </h3>
+              </div>
+              <div>
+                {[...Array(priceRating)].map((_, index) => (
+                  <BiSolidBadgeDollar key={index} className="bd-dollar-sign" />
+                ))}
+              </div>
+              <p className="business-detail-header-text">
+                {businessCategory}
+              </p>
+              <p className="business-detail-header-text schedule-text">
+                {businessSchedule}
+              </p>
+            </div>
+          </div>
 
-      {
-        // TODO: WHOEVER HAS THE TIME TO DEBUG THIS
-        // TODO: THATD BE MUCH APPRECIATED
-        // TODO: PROBABLY EASIER TO DEFINE A COMPONENT FOR THIS LIKE @MoonChopperr
-        // TODO: DID WITH HIS REVIEW WHERE IT'LL NEST THE BUSINESS IMAGES ELSEWHERE
-        // TODO: THEN THIS IS JUST A RENDERING OF IMAGES RATHER
-      }
+          <div className="business-detail-action-buttons-container">
+            <button>Write a review</button>
+            <button>Add a photo</button>
+            <button>Share</button>
+            <button>Save</button>
+            <button>Follow</button>
+          </div>
 
-        {businessImages.length > 0 ? (
-        <div>
-          {businessImages.map(image => (
-          <img key={image.id} src={image.url} alt={`Image ${image.id}`} />
-          ))}
-        </div>) : (<p>No photos available</p>)}
+          <div className="menu-container">
+            <h2>menu</h2>
+          </div>
 
-      <h2>Schedule</h2>
-      <p>{data[businessId].schedule}</p>
-      <h2>Amenities</h2>
-      <h2>Reviews</h2>
-      {reviewArr.map(review => (
-        <p key={review.id}>{review.review}</p>
-      ))}
+          <div className="reviews-container">
+            <h2>reviews</h2>
+            <BusinessReviews />
+          </div>
+
+          <div className="business-info-container schedule-text">
+            <h2>business info</h2>
+          </div>
+
+        </>
+      ) : (
+        <h2>Loading ...</h2>
+      )}
     </>
   )
 }
